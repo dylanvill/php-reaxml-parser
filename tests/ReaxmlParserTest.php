ed9a5475c4a169de70b39e410683125f68d20eca
Dylan Villaruel <dylan@ad-group.com.au>
2025-06-09 12:55:51 +0800

    feat: initial setup for reaxml parser test

---
+++ tests/ReaxmlParserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AdGroup\ReaxmlParser;
+
+use Orchestra\Testbench\TestCase;
+
+class ReaxmlParserTest extends TestCase
+{
+    public function test_sample() {
+        var_dump("test");
+        $this->assertTrue(true);
+    }
+}
