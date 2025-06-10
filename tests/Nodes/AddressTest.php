88c5e07a9f412065dd847cb1f93ba4b49d437fe7
Dylan Villaruel <dylan@ad-group.com.au>
2025-06-10 14:36:21 +0800

    refactor: changed XML generation in address test into trait

--- tests/Nodes/AddressTest.php
+++ tests/Nodes/AddressTest.php
@@ -4,51 +4,84 @@ namespace AdGroup\ReaxmlParser\Tests\Nodes;
 
 use AdGroup\ReaxmlParser\Enums\YesNoEnum;
 use AdGroup\ReaxmlParser\Nodes\Address;
+use AdGroup\ReaxmlParser\Tests\Traits\GeneratesSampleXml;
 use AdGroup\ReaxmlParser\Tests\Traits\TestsNodeValidation;
 use Orchestra\Testbench\PHPUnit\TestCase;
-use SimpleXMLElement;
 
 class AddressTest extends TestCase
 {
-    use TestsNodeValidation;
+    use TestsNodeValidation, GeneratesSampleXml;
 
     private Address $defaultTestAddress;
 
-    private const DEFAULTS = [
-        'subNumber' => '2',
-        'streetNumber' => '39',
-        'street' => 'Main Road',
-        'suburb' => 'RICHMOND',
-        'state' => 'vic',
-        'postcode' => '3121',
-        'country' => 'AUS',
-        'site' => 'test site',
-        'lotNumber' => 'test lot number',
-        'region' => 'test region',
+    const SUB_NUMBER = "2";
+    const STREET_NUMBER = "39";
+    const STREET = "Main Road";
+    const SUBURB = "Richmond";
+    const STATE = "vic";
+    const POSTCODE = "3121";
+    const COUNTRY = "AUS";
+    const SITE = "test site";
+    const LOT_NUMBER = "test lot number";
+    const REGION = "test region";
+
+    private const DEFAULT_CHILD_NODES = [
+        [
+            "name" => "subNumber",
+            "value" => self::SUB_NUMBER,
+            "attributes" => [],
+        ],
+        [
+            "name" => "streetNumber",
+            "value" => self::STREET_NUMBER,
+            "attributes" => [],
+        ],
+        [
+            "name" => "street",
+            "value" => self::STREET,
+            "attributes" => [],
+        ],
+        [
+            "name" => "suburb",
+            "value" => self::SUBURB,
+            "attributes" => [],
+        ],
+        [
+            "name" => "state",
+            "value" => self::STATE,
+            "attributes" => [],
+        ],
+        [
+            "name" => "postcode",
+            "value" => self::POSTCODE,
+            "attributes" => [],
+        ],
+        [
+            "name" => "country",
+            "value" => self::COUNTRY,
+            "attributes" => [],
+        ],
+        [
+            "name" => "site",
+            "value" => self::SITE,
+            "attributes" => [],
+        ],
+        [
+            "name" => "lotNumber",
+            "value" => self::LOT_NUMBER,
+            "attributes" => [],
+        ],
+        [
+            "name" => "region",
+            "value" => self::REGION,
+            "attributes" => [],
+        ],
     ];
 
     public function setUp(): void
     {
         parent::setUp();
-        $this->defaultTestAddress = new Address($this->generateAddressXml());
-    }
-
-    private function generateAddressXml(array $overrides = [], array $attributes = []): SimpleXMLElement
-    {
-        $elements = array_merge(self::DEFAULTS, $overrides);
-        $attrString = '';
-
-        foreach ($attributes as $key => $value) {
-            $attrString .= " {$key}=\"{$value}\"";
-        }
-
-        $xml = "<address{$attrString}>";
-        foreach ($elements as $key => $value) {
-            $xml .= "<{$key}>{$value}</{$key}>";
-        }
-        $xml .= "</address>";
-
-        return simplexml_load_string($xml);
+        $this->defaultTestAddress = new Address($this->generateXml("address", [], self::DEFAULT_CHILD_NODES));
     }
 
     public function nodeClass(): string
@@ -58,52 +91,52 @@ class AddressTest extends TestCase
 
     public function test_has_the_correct_sub_number_value()
     {
-        $this->assertEquals(self::DEFAULTS['subNumber'], $this->defaultTestAddress->subNumber->text);
+        $this->assertEquals(self::SUB_NUMBER, $this->defaultTestAddress->subNumber->text);
     }
 
     public function test_has_the_correct_street_number_value()
     {
-        $this->assertEquals(self::DEFAULTS['streetNumber'], $this->defaultTestAddress->streetNumber->text);
+        $this->assertEquals(self::STREET_NUMBER, $this->defaultTestAddress->streetNumber->text);
     }
 
     public function test_has_the_correct_street_value()
     {
-        $this->assertEquals(self::DEFAULTS['street'], $this->defaultTestAddress->street->text);
+        $this->assertEquals(self::STREET, $this->defaultTestAddress->street->text);
     }
 
     public function test_has_the_correct_suburb_value()
     {
-        $this->assertEquals(self::DEFAULTS['suburb'], $this->defaultTestAddress->suburb->text);
+        $this->assertEquals(self::SUBURB, $this->defaultTestAddress->suburb->text);
     }
 
     public function test_has_the_correct_state_value()
     {
-        $this->assertEquals(self::DEFAULTS['state'], $this->defaultTestAddress->state->text);
+        $this->assertEquals(self::STATE, $this->defaultTestAddress->state->text);
     }
 
     public function test_has_the_correct_postcode_value()
     {
-        $this->assertEquals(self::DEFAULTS['postcode'], $this->defaultTestAddress->postcode->text);
+        $this->assertEquals(self::POSTCODE, $this->defaultTestAddress->postcode->text);
     }
 
     public function test_has_the_correct_country_value()
     {
-        $this->assertEquals(self::DEFAULTS['country'], $this->defaultTestAddress->country->text);
+        $this->assertEquals(self::COUNTRY, $this->defaultTestAddress->country->text);
     }
 
     public function test_has_the_correct_site_value()
     {
-        $this->assertEquals(self::DEFAULTS['site'], $this->defaultTestAddress->site->text);
+        $this->assertEquals(self::SITE, $this->defaultTestAddress->site->text);
     }
 
     public function test_has_the_correct_lot_number_value()
     {
-        $this->assertEquals(self::DEFAULTS['lotNumber'], $this->defaultTestAddress->lotNumber->text);
+        $this->assertEquals(self::LOT_NUMBER, $this->defaultTestAddress->lotNumber->text);
     }
 
     public function test_has_the_correct_region_value()
     {
-        $this->assertEquals(self::DEFAULTS['region'], $this->defaultTestAddress->region->text);
+        $this->assertEquals(self::REGION, $this->defaultTestAddress->region->text);
     }
 
     public function test_display_is_null_when_there_is_no_display_attribute()
@@ -113,28 +146,28 @@ class AddressTest extends TestCase
 
     public function test_display_is_yes_when_display_attribute_is_yes()
     {
-        $xml = $this->generateAddressXml([], ['display' => 'yes']);
+        $xml = $this->generateXml("address", ["display" => "yes"], self::DEFAULT_CHILD_NODES);
         $address = new Address($xml);
         $this->assertEquals(YesNoEnum::YES->value, $address->display->value);
     }
 
     public function test_display_is_no_when_display_attribute_is_no()
     {
-        $xml = $this->generateAddressXml([], ['display' => 'no']);
+        $xml = $this->generateXml("address", ["display" => "no"], self::DEFAULT_CHILD_NODES);
         $address = new Address($xml);
         $this->assertEquals(YesNoEnum::NO->value, $address->display->value);
     }
 
     public function test_streetview_is_yes_when_streetview_attribute_is_yes()
     {
-        $xml = $this->generateAddressXml([], ['streetview' => 'yes']);
+        $xml = $this->generateXml("address", ["streetview" => "yes"], self::DEFAULT_CHILD_NODES);
         $address = new Address($xml);
         $this->assertEquals(YesNoEnum::YES->value, $address->streetview->value);
     }
 
     public function test_streetview_is_no_when_streetview_attribute_is_no()
     {
-        $xml = $this->generateAddressXml([], ['streetview' => 'no']);
+        $xml = $this->generateXml("address", ["streetview" => "no"], self::DEFAULT_CHILD_NODES);
         $address = new Address($xml);
         $this->assertEquals(YesNoEnum::NO->value, $address->streetview->value);
     }
