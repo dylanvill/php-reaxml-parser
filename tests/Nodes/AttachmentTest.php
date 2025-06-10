625e55c7748aac0b08d7702edfdb26bc6d0dadaa
Dylan Villaruel <dylan@ad-group.com.au>
2025-06-10 15:34:58 +0800

    test: finished attachment test

--- tests/Nodes/AttachmentTest.php
+++ tests/Nodes/AttachmentTest.php
@@ -3,13 +3,14 @@
 namespace AdGroup\ReaxmlParser\Tests\Nodes;
 
 use AdGroup\ReaxmlParser\Nodes\Attachment;
+use AdGroup\ReaxmlParser\Tests\Traits\GeneratesSampleXml;
 use AdGroup\ReaxmlParser\Tests\Traits\TestsNodeValidation;
 use AdGroup\ReaxmlParser\Tests\Traits\TestsTextNode;
 use Orchestra\Testbench\TestCase;
 
 class AttachmentTest extends TestCase
 {
-    use TestsNodeValidation, TestsTextNode;
+    use TestsNodeValidation, TestsTextNode, GeneratesSampleXml;
 
     protected function nodeName(): string
     {
@@ -19,5 +20,53 @@ class AttachmentTest extends TestCase
     protected function nodeClass(): string
     {
         return Attachment::class;
+    }
+
+    public function test_id_is_null_when_there_is_no_id_attribute(): void
+    {
+        $xml = $this->generateXml(Attachment::NODE_NAME);
+        $attachment = new Attachment($xml);
+
+        $this->assertNull($attachment->id);
+    }
+
+    public function test_id_has_the_correct_value(): void
+    {
+        $xml = $this->generateXml(Attachment::NODE_NAME, ['id' => 'id-value']);
+        $attachment = new Attachment($xml);
+
+        $this->assertEquals('id-value', $attachment->id);
+    }
+
+    public function test_usage_is_null_when_there_is_no_usage_attribute(): void
+    {
+        $xml = $this->generateXml(Attachment::NODE_NAME);
+        $attachment = new Attachment($xml);
+
+        $this->assertNull($attachment->usage);
+    }
+
+    public function test_usage_has_the_correct_value(): void
+    {
+        $xml = $this->generateXml(Attachment::NODE_NAME, ['usage' => 'statementOfInformation']);
+        $attachment = new Attachment($xml);
+
+        $this->assertEquals('statementOfInformation', $attachment->usage);
+    }
+
+    public function test_url_is_null_when_there_is_no_url_attribute(): void
+    {
+        $xml = $this->generateXml(Attachment::NODE_NAME);
+        $attachment = new Attachment($xml);
+
+        $this->assertNull($attachment->url);
+    }
+
+    public function test_url_has_the_correct_value(): void
+    {
+        $xml = $this->generateXml(Attachment::NODE_NAME, ['url' => 'test-url']);
+        $attachment = new Attachment($xml);
+
+        $this->assertEquals('test-url', $attachment->url);
     }
 }
