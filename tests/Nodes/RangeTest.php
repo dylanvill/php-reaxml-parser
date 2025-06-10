89a7bc1ed2eb8a190ac8fa6c10c2015a09dcf94c
Dylan Villaruel <dylan@ad-group.com.au>
2025-06-10 14:12:34 +0800

    test: finished tests for range

--- tests/Nodes/RangeTest.php
+++ tests/Nodes/RangeTest.php
@@ -20,10 +20,35 @@ class RangeTest extends TestCase
 
     public function test_min_and_max_is_null_when_there_are_no_child_nodes(): void
     {
-        $xml = $this->generateXml('range', []);
+        $xml = $this->generateXml('range', [], []);
         $range = new Range($xml);
 
         $this->assertNull($range->min);
         $this->assertNull($range->max);
+    }
+
+    public function test_min_and_max_value_contains_the_correct_value(): void
+    {
+        $xml = $this->generateXml(
+            'range',
+            [],
+            [
+                [
+                    'name' => 'min',
+                    'value' => '1',
+                    'attributes' => []
+                ],
+                [
+                    'name' => 'max',
+                    'value' => '10',
+                    'attributes' => []
+                ]
+            ]
+        );
+
+        $range = new Range($xml);
+
+        $this->assertEquals("1", $range->min->text);
+        $this->assertEquals("10", $range->max->text);
     }
 }
