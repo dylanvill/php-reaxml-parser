f0d0eb0c4b82e3a83171bf880279a854ee71d357
Dylan Villaruel <dylan@ad-group.com.au>
2025-06-10 08:11:23 +0800

    refactor: site test to use traits

--- tests/Nodes/SiteTest.php
+++ tests/Nodes/SiteTest.php
@@ -2,34 +2,22 @@
 
 namespace AdGroup\ReaxmlParser\Tests\Nodes;
 
-use AdGroup\ReaxmlParser\Exceptions\IncorrectNodeArgument;
 use AdGroup\ReaxmlParser\Nodes\Site;
+use AdGroup\ReaxmlParser\Tests\Traits\TestsNodeValidation;
+use AdGroup\ReaxmlParser\Tests\Traits\TestsTextNode;
 use Orchestra\Testbench\TestCase;
 
 class SiteTest extends TestCase
 {
-    public function test_text_from_node_is_correct(): void
-    {
-        $text = "sample site text";
-        $xml = simplexml_load_string("<site>{$text}</site>");
-        $instance = new Site($xml);
+    use TestsTextNode, TestsNodeValidation;
 
-        $this->assertEquals($text, $instance->text);
+    public function nodeName(): string
+    {
+        return 'site';
     }
 
-    public function test_text_is_null_when_node_does_not_have_any_text(): void
+    public function nodeClass(): string
     {
-        $xml = simplexml_load_string("<site></site>");
-        $instance = new Site($xml);
-
-        $this->assertNull($instance->text);
-    }
-
-    public function test_throws_error_when_node_received_is_incorrect(): void
-    {
-        $this->expectException(IncorrectNodeArgument::class);
-
-        $xml = simplexml_load_string("<wrongNode></wrongNode>");
-        new Site($xml);
+        return Site::class;
     }
 }
