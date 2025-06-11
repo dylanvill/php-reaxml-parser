e72c9e8bd94eae072fa3303a8a17e3bf32645072
Dylan Villaruel <dylan@ad-group.com.au>
2025-06-11 13:18:02 +0800

    feat: changed generate sample xml trait to accommodate recursiveness

--- tests/Traits/GeneratesSampleXml.php
+++ tests/Traits/GeneratesSampleXml.php
@@ -6,25 +6,54 @@ use SimpleXMLElement;
 
 trait GeneratesSampleXml
 {
+
+    /**
+     * Generates a dummy / test XML.
+     *
+     * @param string $parentNodeName The name of the root node of the XML.
+     * @param array $attributes The attributes of the root node.
+     * @param array $childNodes An array of arrays with the following keys:
+     * - `name` Name of the node
+     * - `value` The value of the node, can be an array with the same format 
+     * - `attributes` The attributes of the node
+     * @return SimpleXMLElement
+     */
     private function generateXml(string $parentNodeName, array $attributes = [], array $childNodes = []): SimpleXMLElement
     {
 
-        $parentAttributes = '';
-        foreach ($attributes as $key => $value) {
-            $parentAttributes .= " {$key}=\"{$value}\"";
-        }
+        $parentAttributes = $this->parseAttributes($attributes);
 
         $content = "<{$parentNodeName}{$parentAttributes}>";
 
         foreach ($childNodes as $node) {
-            $childAttributes = '';
-            foreach ($node['attributes'] as $key => $value) {
-                $childAttributes .= " {$key}=\"{$value}\"";
-            }
-            $content .= "<{$node['name']}{$childAttributes}>{$node['value']}</{$node['name']}>";
+            $content .= $this->parseChildNode($node);
         }
+
         $content .= "</{$parentNodeName}>";
 
         return simplexml_load_string($content);
+    }
+
+    private function parseChildNode(array $node): string
+    {
+        $name = $node['name'];
+        $attributes = $this->parseAttributes($node['attributes'] ?? []);
+        $value = $node['value'];
+
+        if (is_array($value) && isset($value['name'], $value['value'])) {
+            $value = $this->parseChildNode($value);
+        }
+
+        return "<{$name}{$attributes}>{$value}</{$name}>";
+    }
+
+    private function parseAttributes(array $attributes): string
+    {
+        $parsedAttribute = '';
+        foreach ($attributes as $key => $value) {
+            $parsedAttribute .= " {$key}=\"{$value}\"";
+        }
+
+        return $parsedAttribute;
     }
 }
