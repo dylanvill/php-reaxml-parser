b68e348fd43ebd491123143d25beb3259fd52cd3
Dylan Villaruel <dylan@ad-group.com.au>
2025-06-11 14:26:06 +0800

    refactor: used xml generator trait

--- tests/Nodes/AllowancesTest.php
+++ tests/Nodes/AllowancesTest.php
@@ -6,27 +6,14 @@ use AdGroup\ReaxmlParser\Nodes\Allowances;
 use AdGroup\ReaxmlParser\Nodes\Furnished;
 use AdGroup\ReaxmlParser\Nodes\PetFriendly;
 use AdGroup\ReaxmlParser\Nodes\Smokers;
+use AdGroup\ReaxmlParser\Tests\Traits\GeneratesSampleXml;
 use Orchestra\Testbench\TestCase;
 use AdGroup\ReaxmlParser\Tests\Traits\TestsNodeValidation;
 use SimpleXMLElement;
 
 class AllowancesTest extends TestCase
 {
-    use TestsNodeValidation;
-
-    const TEST_VALUE = "yes";
-
-    private function generateAllowancesXml(array $childElements = []): SimpleXMLElement
-    {
-        $content = "<allowances>";
-
-        foreach ($childElements as $key => $value) {
-            $content .= "<{$key}>{$value}</{$key}>";
-        }
-        $content .= "</allowances>";
-
-        return simplexml_load_string($content);
-    }
+    use TestsNodeValidation, GeneratesSampleXml;
 
     protected function nodeClass(): string
     {
@@ -35,7 +22,7 @@ class AllowancesTest extends TestCase
 
     public function test_all_properties_are_null_when_allowances_xml_is_empty(): void
     {
-        $xml = $this->generateAllowancesXml();
+        $xml = $this->generateXml(Allowances::NODE_NAME);
         $allowances = new Allowances($xml);
 
         $this->assertNull($allowances->petFriendly);
@@ -45,25 +32,43 @@ class AllowancesTest extends TestCase
 
     public function test_pet_friendly_contains_the_correct_text_value(): void
     {
-        $xml = $this->generateAllowancesXml([PetFriendly::NODE_NAME => self::TEST_VALUE]);
+        $xml = $this->generateXml(Allowances::NODE_NAME, [], [
+            [
+                "name" => PetFriendly::NODE_NAME,
+                "value" => "pet-friendly-test",
+                "attributes" => []
+            ]
+        ]);
         $allowances = new Allowances($xml);
 
-        $this->assertEquals(self::TEST_VALUE, $allowances->petFriendly->text);
+        $this->assertEquals("pet-friendly-test", $allowances->petFriendly->text);
     }
 
     public function test_furnished_contains_the_correct_text_value(): void
     {
-        $xml = $this->generateAllowancesXml([Furnished::NODE_NAME => self::TEST_VALUE]);
+        $xml = $this->generateXml(Allowances::NODE_NAME, [], [
+            [
+                "name" => Furnished::NODE_NAME,
+                "value" => "furnished-test",
+                "attributes" => []
+            ]
+        ]);
         $allowances = new Allowances($xml);
 
-        $this->assertEquals(self::TEST_VALUE, $allowances->furnished->text);
+        $this->assertEquals("furnished-test", $allowances->furnished->text);
     }
 
     public function test_smokers_contains_the_correct_text_value(): void
     {
-        $xml = $this->generateAllowancesXml([Smokers::NODE_NAME => self::TEST_VALUE]);
+        $xml = $this->generateXml(Allowances::NODE_NAME, [], [
+            [
+                "name" => Smokers::NODE_NAME,
+                "value" => "smokers-test",
+                "attributes" => []
+            ]
+        ]);
         $allowances = new Allowances($xml);
 
-        $this->assertEquals(self::TEST_VALUE, $allowances->smokers->text);
+        $this->assertEquals("smokers-test", $allowances->smokers->text);
     }
 }
--- tests/Nodes/SuburbTest.php
+++ tests/Nodes/SuburbTest.php
@@ -4,13 +4,14 @@ namespace AdGroup\ReaxmlParser\Tests\Nodes;
 
 use AdGroup\ReaxmlParser\Enums\YesNoEnum;
 use AdGroup\ReaxmlParser\Nodes\Suburb;
+use AdGroup\ReaxmlParser\Tests\Traits\GeneratesSampleXml;
 use AdGroup\ReaxmlParser\Tests\Traits\TestsNodeValidation;
 use AdGroup\ReaxmlParser\Tests\Traits\TestsTextNode;
 use Orchestra\Testbench\TestCase;
 
 class SuburbTest extends TestCase
 {
-    use TestsTextNode, TestsNodeValidation;
+    use TestsTextNode, TestsNodeValidation, GeneratesSampleXml;
 
     protected function nodeName(): string
     {
@@ -24,8 +25,7 @@ class SuburbTest extends TestCase
 
     public function test_display_is_null_when_no_attribute_is_present()
     {
-        $xml = simplexml_load_string("<suburb>Suburb</suburb>");
-
+        $xml = $this->generateXml(Suburb::NODE_NAME);
         $node = new Suburb($xml);
 
         $this->assertNull($node->display);
@@ -33,8 +33,7 @@ class SuburbTest extends TestCase
 
     public function test_display_is_yes_when_attribute_is_yes()
     {
-        $xml = simplexml_load_string("<suburb display='yes'>Suburb</suburb>");
-
+        $xml = $this->generateXml(Suburb::NODE_NAME, ["display" => "yes"]);
         $node = new Suburb($xml);
 
         $this->assertEquals(YesNoEnum::YES->value, $node->display->value);
@@ -42,8 +41,7 @@ class SuburbTest extends TestCase
 
     public function test_display_is_no_when_attribute_is_no()
     {
-        $xml = simplexml_load_string("<suburb display='no'>Suburb</suburb>");
-
+        $xml = $this->generateXml(Suburb::NODE_NAME, ["display" => "no"]);
         $node = new Suburb($xml);
 
         $this->assertEquals(YesNoEnum::NO->value, $node->display->value);
@@ -51,8 +49,7 @@ class SuburbTest extends TestCase
 
     public function test_display_property_is_instance_of_yes_no_enum()
     {
-        $xml = simplexml_load_string("<suburb display='no'>Suburb</suburb>");
-
+        $xml = $this->generateXml(Suburb::NODE_NAME, ["display" => "no"]);
         $node = new Suburb($xml);
 
         $this->assertInstanceOf(YesNoEnum::class, $node->display);
