218c11bf97bb7e8595b9d56b9f6d61d8a84fc456
Dylan Villaruel <dylan@ad-group.com.au>
2025-06-12 09:58:13 +0800

    feat, test: added range per square metre mapping and test

--- src/Nodes/RentPerSquareMetre.php
+++ src/Nodes/RentPerSquareMetre.php
@@ -4,15 +4,25 @@ namespace AdGroup\ReaxmlParser\Nodes;
 
 use AdGroup\ReaxmlParser\Traits\HasText;
 use AdGroup\ReaxmlParser\Nodes\Range;
+use AdGroup\ReaxmlParser\Traits\HasNodeValidation;
 use SimpleXMLElement;
 
 class RentPerSquareMetre
 {
     const NODE_NAME = "rentPerSquareMetre";
 
-    use HasText;
+    use HasText, HasNodeValidation;
 
     public ?Range $range = null;
 
-    public function __construct(SimpleXMLElement $node) {}
+    public function __construct(SimpleXMLElement $node)
+    {
+        $this->validateNodeName(self::NODE_NAME, $node);
+
+        $rangeElement = $node->xpath(Range::NODE_NAME);
+
+        if (!empty($rangeElement)) {
+            $this->range = new Range($rangeElement[0]);
+        }
+    }
 }
---
+++ tests/Nodes/RangePerSquareMetreTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AdGroup\ReaxmlParser\Tests\Nodes;
+
+use AdGroup\ReaxmlParser\Nodes\CommercialListingType;
+use AdGroup\ReaxmlParser\Nodes\RentPerSquareMetre;
+use AdGroup\ReaxmlParser\Tests\Traits\GeneratesSampleXml;
+use Orchestra\Testbench\TestCase;
+use AdGroup\ReaxmlParser\Tests\Traits\TestsNodeValidation;
+
+class RangePerSquareMetreTest extends TestCase
+{
+    use TestsNodeValidation, GeneratesSampleXml;
+
+    protected function nodeClass(): string
+    {
+        return RentPerSquareMetre::class;
+    }
+
+    public function test_range_is_null_when_there_is_no_range_child_element(): void
+    {
+        $xml = $this->generateXml(RentPerSquareMetre::NODE_NAME);
+        $rentPerSquareMetre = new RentPerSquareMetre($xml);
+
+        $this->assertNull($rentPerSquareMetre->range);
+    }
+
+    public function test_range_is_not_null_when_range_is_present(): void
+    {
+        $xml = $this->generateXml(RentPerSquareMetre::NODE_NAME, [], [
+            [
+                "name" => "range",
+                "attributes" => [],
+                "value" => ""
+            ]
+        ]);
+        $rentPerSquareMetre = new RentPerSquareMetre($xml);
+
+        $this->assertNotNull($rentPerSquareMetre->range);
+    }
+}
