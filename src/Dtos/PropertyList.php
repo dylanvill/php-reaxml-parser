545e1c45647d5f95340bbd556e02a5df721b8680
Dylan Villaruel <thraxxdv@gmail.com>
2025-06-25 07:59:57 +0800

    feat: added noListings method in property list dto

--- src/Dtos/PropertyList.php
+++ src/Dtos/PropertyList.php
@@ -10,4 +10,14 @@ class PropertyList
     public array $rental = [];
     public array $residential = [];
     public array $rural = [];
+
+    public function noListings(): bool
+    {
+        return empty($commercial) &&
+            empty($holidayRental) &&
+            empty($land) &&
+            empty($rental) &&
+            empty($residential) &&
+            empty($rural);
+    }
 }
