5991b8eeb69deda5bf567b5461c97dafb723d931
Dylan Villaruel <thraxxdv@gmail.com>
2025-06-26 12:23:17 +0800

    feat: added attributes in property list dto

--- src/Dtos/PropertyList.php
+++ src/Dtos/PropertyList.php
@@ -5,6 +5,13 @@ namespace AdGroup\ReaxmlParser\Dtos;
 readonly class PropertyList
 {
     public function __construct(
+        public ?string $date = null,
+        public ?string $companyName = null,
+        public ?string $techContactName = null,
+        public ?string $techContactPhone = null,
+        public ?string $techContactEmail = null,
+        public ?string $username = null,
+        public ?string $password = null,
         public array $commercial = [],
         public array $holidayRental = [],
         public array $land = [],
